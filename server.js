37c89d17c46217fba8c33f2b37f65b8876fa8836
3dm4rk <edmarksumampen@gmail.com>
2026-05-21 02:16:45 +0800

    Initialize server with TikTok live connection

---
+++ server.js
@@ -0,0 +1,35 @@
+const { WebcastPushConnection } = require('tiktok-live-connector');
+const express = require('express');
+
+const app = express();
+
+let latestComment = "";
+
+const tiktokUsername = "edmark.jay121";
+
+const tiktokLiveConnection = new WebcastPushConnection(tiktokUsername);
+
+tiktokLiveConnection.connect()
+    .then(state => {
+        console.log(`Connected to roomId ${state.roomId}`);
+    })
+    .catch(err => {
+        console.error('Failed to connect', err);
+    });
+
+tiktokLiveConnection.on('chat', data => {
+
+    console.log(`${data.uniqueId}: ${data.comment}`);
+
+    latestComment = `${data.uniqueId}:${data.comment}`;
+});
+
+app.get('/comment', (req, res) => {
+    res.json({
+        comment: latestComment
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
